7f370cba81e961e1dd951816b4cc0a700bc6c7af
admin <admin@DESKTOP-UBR9SPO>
2024-09-23 16:46:12 +0900

    Test 추가.

---
+++ Kingz/src/main/java/com/yedam/example/control/TestControl.java
@@ -0,0 +1,19 @@
+package com.yedam.example.control;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yedam.common.Control;
+
+public class TestControl implements Control {
+
+	@Override
+	public void exec(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
