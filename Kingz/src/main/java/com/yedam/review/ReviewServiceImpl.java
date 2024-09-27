9b340583a36b7525477d643e8d1cf3fe9659a0d7
jms <admin@DESKTOP-12B7EAS>
2024-09-27 12:47:06 +0900

    조민성 7

--- Kingz/src/main/java/com/yedam/review/ReviewServiceImpl.java
+++ Kingz/src/main/java/com/yedam/review/ReviewServiceImpl.java
@@ -8,10 +8,9 @@ import com.yedam.common.DataSource;
 import com.yedam.review.mapper.ReviewMapper;
 
 public class ReviewServiceImpl implements ReviewService {
-	
+
 	SqlSession sqlSession = DataSource.getInstance().openSession(true);
 	ReviewMapper mapper = sqlSession.getMapper(ReviewMapper.class);
-//	ReservMapper map = sqlSession.getMapper(ReservMapper.class);
 
 	@Override
 	public List<ReviewVO> getReviewList(int roomId) {
@@ -20,28 +19,9 @@ public class ReviewServiceImpl implements ReviewService {
 
 	@Override
 	public boolean addReview(ReviewVO rvo) {
-		
-//		ReservVO reservation = map.reviewCheck(rvo.getRoomId(), rvo.getMemberId());
-//		if(reservation != null) {
-//		// 예약 번호로 이미 리뷰가 등록되었는지 확인
-//      	ReviewVO existingReview = reviewMapper.getReviewByReserveId(reservation.getReserveId());
-//        
-//        	if (existingReview == null) {
-//            	// 리뷰가 아직 등록되지 않은 경우에만 리뷰 등록
-//            	rvo.setReserveId(reservation.getReserveId());
-//            	return reviewMapper.insertReview(rvo) == 1;
-//        	} else {
-//            	// 이미 리뷰가 등록된 경우
-//            	return false;
-//        	}
-//    	} else {
-//        	// 체크아웃이 지나지 않았거나 예약 정보가 없는 경우
-//        	return false;
-//   	}
-			
 		return mapper.insertReview(rvo) == 1;
 	}
-	
+
 	@Override
 	public boolean addImage(ImageVO ivo) {
 		return mapper.insertImage(ivo) == 1;
@@ -50,6 +30,21 @@ public class ReviewServiceImpl implements ReviewService {
 	@Override
 	public int getReviewId() {
 		return mapper.selectReviewId();
+	}
+
+	@Override
+	public int reviewCheck(int roomId, String memberId) {
+		return mapper.reviewCheck(roomId, memberId);
+	}
+
+	@Override
+	public int getReserveCnt(String memberId, int roomId) {
+		return mapper.countReserve(memberId, roomId);
+	}
+
+	@Override
+	public int getReviewCnt(String memberId, int roomId) {
+		return mapper.countReview(memberId, roomId);
 	}
 
 }
